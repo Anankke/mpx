d003bd26691a11b0718e7c9acf0a45896f20ed09
Anankke <anankke@pm.me>
2025-05-19 04:04:17 -0700

    adjust loglevel

--- mpx-tunnel/main.go
+++ mpx-tunnel/main.go
@@ -13,7 +13,6 @@ import (
 	"github.com/Anankke/mpx"
 	"github.com/Anankke/mpx/dialer"
 	"github.com/Anankke/mpx/mpx-tunnel/relay"
-	"github.com/Anankke/mpx/debug"
 )
 
 var (
@@ -165,7 +164,9 @@ func runServer(localAddr, targetAddr string) {
 		}
 		go func() {
 			defer tunn.Close()
-			debug.Printf("dial to %s", targetAddr)
+			if *verbose {
+				log.Printf("dial to %s", targetAddr)
+			}
 			rc, err := net.DialTCP("tcp", nil, target)
 			if err != nil || rc == nil {
 				log.Printf("failed to connect to target[%s]: %v", targetAddr, err)
